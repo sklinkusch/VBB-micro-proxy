10b2fe7b71f061558bb366392b99db9bf5539dce
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-01-23 21:17:20 +0100

    add function splitArray

--- manipulateData/index.js
+++ manipulateData/index.js
@@ -45,3 +45,74 @@ function sortData = (data) => {
   }
   return []
 }
+
+async function splitArray (data) {
+  if (data !== undefined && data.length > 0) {
+    const dataModified = data.map((e) => {
+      const newStopObject = changeStopObject(mode, e)
+      return newStopObject
+    })
+    const stopsRaw = await dataModified.map((e) => ({
+			name: e.stop.name,
+			order: e.order,
+		}))
+    const intermediateArray = await stopsRaw.reduce(
+			(acc: Intermediate[], item: Intermediate) => {
+				const arr = acc.slice()
+				const i = arr.findIndex(
+					(x) => x.name === item.name && x.order === item.order
+				)
+				if (i <= -1) arr.push(item)
+					return arr
+			},
+			[]
+		)
+    const stopsContracted = await intermediateArray.sort((a, b) => {
+			const { name: aName, order: aOrder } = a
+			const { name: bName, order: bOrder } = b
+			if (
+				typeof aOrder === "number" &&
+				typeof bOrder === "number" &&
+				aOrder < bOrder
+			)
+				return -1
+			if (
+				typeof aOrder === "number" &&
+				typeof bOrder === "number" &&
+				bOrder < aOrder
+			)
+				return +1
+			if (
+				typeof aName === "string" &&
+				typeof bName === "string" &&
+				aName.toLowerCase() < bName.toLowerCase()
+			)
+				return -1
+			if (
+				typeof bName === "string" &&
+				typeof aName === "string" &&
+				bName.toLowerCase() < aName.toLowerCase()
+			)
+				return +1
+			return 0
+		})
+    const resultArray = await dataModified.reduce(
+			(acc: Dataset[][], curr: any) => {
+				const arr = [...acc]
+				const index = stopsContracted.findIndex(
+					(x) => x.name === curr.stop.name && x.order === curr.order
+			  )
+				if (Array.isArray(arr[index])) {
+					arr[index].push(curr)
+				} else {
+					arr[index] = [curr]
+				}
+				return arr
+			},
+			[]
+		)
+		if ((await resultArray.length) > 0) {
+			return await resultArray
+		}
+  }
+}
