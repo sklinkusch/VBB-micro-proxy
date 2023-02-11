4684aab369617c89e4124e611d05bfcde2f9f04d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-02-11 16:10:51 +0100

    fix line filter

--- dataFilter.js
+++ dataFilter.js
@@ -1,4 +1,4 @@
-const filterProducts = (data, products) => {
+const filterProducts = (data, products = "1111111") => {
   if (typeof products === "string" && String.length === 7) {
     const productsArray = products.split("");
     const modProductsArray = productsArray.map((item) => {
@@ -31,17 +31,21 @@ const filterProducts = (data, products) => {
   }
 };
 
-const lineFilter = (data, lines) => {
-  const lineArray = lines.split(",");
-  const smallLineArray = lineArray.map((singleLine) =>
-    singleLine.toLowerCase()
-  );
-  if (smallLineArray.length > 0) {
-    const returnData = data.filter((dataset) => {
-      const currentLine = dataset.line.name.toLowerCase();
-      return smallLineArray.includes(currentLine);
-    });
-    return returnData;
+const lineFilter = (data, lines = "") => {
+  if (typeof lines === "string") {
+    const lineArray = lines.split(",");
+    const smallLineArray = lineArray.map((singleLine) =>
+      singleLine.toLowerCase()
+    );
+    if (smallLineArray.length >= 1 && smallLineArray[0] !== "") {
+      const returnData = data.filter((dataset) => {
+        const currentLine = dataset.line.name.toLowerCase();
+        return smallLineArray.includes(currentLine);
+      });
+      return returnData;
+    } else {
+      return data;
+    }
   } else {
     return data;
   }
